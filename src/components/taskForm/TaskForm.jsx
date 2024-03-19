2d4cb6c8507926b4ba4a3c43d0aa39723678ac62
Rafiqul Islam Tusher <md.rafiqul26997@gmail.com>
2024-03-19 13:03:08 +0600

    create a common form component for both task add and create and add new task with this successfully

---
+++ src/components/taskForm/TaskForm.jsx
@@ -0,0 +1,93 @@
+// TaskForm.js (example component)
+
+import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import InputError from '../inputError/InputError';
+import { addTask, updateTask } from '../../features/tasks/tasksSlice'; // Import the addTask and updateTask actions
+
+const TaskForm = ({ selectedTask }) => {
+    const navigate = useNavigate() // to navigate to the another page
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+      reset
+    } = useForm() // to handle form submission
+    
+    const dispatch = useDispatch();
+
+    const onSubmit = (data) => {
+        if (!data.name) return; // Prevent submission if task name is empty    
+        const { name, description } = data // destructured the data object
+
+        if (selectedTask) {
+            // If selectedTask exists, Then edit operation
+            dispatch(updateTask({ id: selectedTask.id, ...data })); // Dispatch updateTask action
+        } else {
+            const newTask = {
+                id: Math.random().toString(36).substr(2, 9), // giving a unique id
+                name: name,
+                description: description,
+                completed: false,
+            };
+            dispatch(addTask(newTask)); // Dispatch addTask action
+            navigate('/task-list') // once the new task added navigate            
+        }
+
+        // Reset form after submission
+        reset();
+    };
+
+    return (
+        <>
+            {/* <form onSubmit={handleSubmit(onSubmit)}>
+                <input
+                    type="text"
+                    name="name"
+                    ref={register}
+                    defaultValue={selectedTask ? selectedTask.name : ''}
+                    placeholder="Task name"
+                />
+                <textarea
+                    name="description"
+                    ref={register}
+                    defaultValue={selectedTask ? selectedTask.description : ''}
+                    placeholder="Task description"
+                />
+                <button type="submit">{selectedTask ? 'Update Task' : 'Add Task'}</button>
+            </form> */}
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <label htmlFor="name" className="form-label">Task Name</label>
+                    <div className="input-group mb-3">
+                        <input
+                            type="text"
+                            className="form-control"
+                            defaultValue={selectedTask ? selectedTask.name : ''}
+                            {...register("name", { required: 'This is required' })}
+                        />
+                    </div>
+                    {errors?.name && <InputError message={errors.name.message} />}
+                </div>
+                <div>
+                    <label htmlFor="name" className="form-label">Description</label>
+                    <div className="input-group mb-3">
+                    <input
+                        type="textarea"
+                        className="form-control"
+                        defaultValue={selectedTask ? selectedTask.description : ''}
+                        {...register("description")}
+                    />
+                    </div>
+                    {errors?.description && <InputError message={errors.description.message} />}
+                </div>
+                
+                <button type="submit" className="btn btn-primary">Save</button>
+            </form>
+        </>
+        
+    );
+};
+
+export default TaskForm;
--- src/pages/addTask/AddNewTask.jsx
+++ src/pages/addTask/AddNewTask.jsx
@@ -1,73 +1,25 @@
-import { useForm } from 'react-hook-form';
-import InputError from '../../components/inputError/InputError';
-import { useDispatch, useSelector } from 'react-redux';
-import { addTask } from '../../features/tasks/tasksSlice';
+import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import TopHeader from '../../components/header/TopHeader';
 import Button from '../../components/button/Button';
+import TaskForm from '../../components/taskForm/TaskForm';
+import { Fragment } from 'react';
 
 const AddNewTask = () => {
   const navigate = useNavigate() // to navigate to the another page
   const tasks = useSelector((state) => state.tasks.allTasks) // get all tasks from redux state
-  const dispatch = useDispatch() 
 
   console.log(tasks)
-  const {
-      register,
-      handleSubmit,
-      formState: { errors },
-      reset
-  } = useForm() // to handle form submission
 
-  const onSubmit = (data) => {
-    const { name, description } = data // destructured the data object
-    if (data) {
-      const newTask = {
-          id: Math.random().toString(36).substr(2, 9), // giving a unique id
-          name: name,
-          description: description,
-          completed: false,
-      };
-      dispatch(addTask(newTask)) // dispatching the add new task reducer
-      navigate('/task-list') // once the new task added navigate
-      reset(); // reset the entire form
-    }
-  };
   return (
-    <div className="">
+    <Fragment>
       <TopHeader title='Add Tasks'>
         <Button onClick={()=>navigate('/task-list')} type='button' className='primary'>Task List</Button>
       </TopHeader>   
       <div className="task-add-form">
-         <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-                <label htmlFor="name" className="form-label">Task Name</label>
-                <div className="input-group mb-3">
-                  <input
-                    type="text"
-                    className="form-control"
-                    {...register("name", { required: 'This is required' })}
-                  />
-                </div>
-                {errors?.name && <InputError message={errors.name.message} />}
-              </div>
-              <div>
-                <label htmlFor="name" className="form-label">Description</label>
-                <div className="input-group mb-3">
-                  <input
-                    type="textarea"
-                    className="form-control"
-                    {...register("description")}
-                  />
-                </div>
-                {errors?.description && <InputError message={errors.description.message} />}
-            </div>
-            
-            <button type="submit" className="btn btn-primary">Save</button>
-        </form>
-      </div>
-     
-    </div>
+        <TaskForm/>
+      </div>     
+    </Fragment>
      
     );
 };
